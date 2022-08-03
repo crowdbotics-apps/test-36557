54d14e350db3a215428d6e2d91433654a95a9050
Crowdbotics <team@crowdbotics.com>
2022-08-03 09:50:42 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,10 @@
+from django.conf import settings
 from django.db import models
+
+
+class Products(models.Model):
+    "Generated Model"
+    p_name = models.CharField(
+        max_length=256,
+    )
+    mrp = models.FloatField()
